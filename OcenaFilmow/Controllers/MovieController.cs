b4866665f4a4675c1b100e1f95b331641631d834
Jakub Żemajduk <kubazemajduk1@gmail.com>
2024-12-02 14:49:38 +0100

    Refactor controller to align with MVC

--- OcenaFilmow/Controllers/MovieController.cs
+++ OcenaFilmow/Controllers/MovieController.cs
@@ -16,7 +16,6 @@ namespace OcenaFilmow.Controllers
         {
             _context = context;
         }
-
         public void AddMovie(string title, string genre, int yearOfProduction, double rating, int numberOfRatings)
         {
             var movie = new Movie(title, genre, yearOfProduction, rating, numberOfRatings);
@@ -37,53 +36,14 @@ namespace OcenaFilmow.Controllers
                 .Take(topCount)
                 .ToList();
         }
-       public static void DisplayMovies(MovieController movieController, List<Movie> movies)
+
+        public  List<Movie> GetPageMovies(List<Movie> movies, int pageNumber)
         {
-            int pageNumber = 1;
-            const int pageSize = 10;
-            bool exitPagination = false;
-            List<Movie> filteredMovies = movies;
-
-            while (!exitPagination)
-            {
-                var pageMovies = filteredMovies
-                .Skip((pageNumber - 1) * pageSize)
-                .Take(pageSize)
+            int PageSize = 10;
+            return movies
+                .Skip((pageNumber - 1) * PageSize)
+                .Take(PageSize)
                 .ToList();
-
-                Console.Clear();
-                Console.WriteLine($"Page {pageNumber}:\n");
-
-                if (pageMovies.Count == 0 && pageNumber > 1)
-                {
-                    Console.WriteLine("No more movies to display.");
-                    pageNumber--;
-                    continue;
-                }
-
-                MovieView.ShowMovies(pageMovies);
-                Console.WriteLine("\n Press 'b' for Previous page, 'n' for Next page, 'f' for Filter or 'q' to Quit.");
-                var action = Console.ReadKey(intercept: true).KeyChar;
-
-                switch (action)
-                {
-                    case 'n':
-                        pageNumber++;
-                        break;
-                    case 'b':
-                        if (pageNumber > 1)
-                            pageNumber--;
-                        break;
-                    case 'f':
-                        filteredMovies = ApplyFilters(movieController, movies);
-                        pageNumber = 1;
-                        break;
-                    case 'q':
-                        exitPagination = true;
-                        break;
-                }
-                Console.Clear();
-            }
         }
         public List<Movie> SearchMoviesByTitle(string titlePrefix)
         {
@@ -93,110 +53,23 @@ namespace OcenaFilmow.Controllers
                            .Take(10)
                            .ToList();
         }
-
-        public string SearchMovies(MovieController movieController)
+        public bool AddToLikedMovies(int userId, int movieId)
         {
-            Console.Clear();
-            Console.WriteLine("Search for a movie by typing its title. Press 'q' to quit.");
+            bool alreadyLiked = _context.LikedMovies.Any(lm => lm.UserId == userId && lm.MovieId == movieId);
 
-            string searchTerm = string.Empty;
-            bool searching = true;
-
-            while (searching)
+            if (alreadyLiked)
             {
-                Console.SetCursorPosition(0, 2);
-                Console.Write("Enter title: " + searchTerm.PadRight(Console.WindowWidth - 11));
-                var keyInfo = Console.ReadKey(intercept: true);
-
-                if (keyInfo.Key == ConsoleKey.Backspace && searchTerm.Length > 0)
-                {
-                    searchTerm = searchTerm.Substring(0, searchTerm.Length - 1);
-                }
-                else if (keyInfo.Key == ConsoleKey.Q)
-                {
-                    searching = false;
-                    break;
-                }
-                else if (keyInfo.Key == ConsoleKey.Enter)
-                {
-                    return searchTerm;
-                }
-                else if (!char.IsControl(keyInfo.KeyChar))
-                {
-                    searchTerm += keyInfo.KeyChar;
-                }
-
-                Console.SetCursorPosition(0, 4);
-                Console.WriteLine("Matching movies:\n");
-
-                var matchingMovies = movieController.SearchMoviesByTitle(searchTerm);
-
-                if (matchingMovies.Any())
-                {
-                    foreach (var movie in matchingMovies)
-                    {
-                        Console.WriteLine($"{movie.Title} ({movie.YearOfProduction}) - Rating: {movie.Rating}");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("No matching movies found.");
-                }
-
-                int linesToClear = 10 - matchingMovies.Count;
-                for (int i = 0; i < linesToClear; i++)
-                {
-                    Console.WriteLine(new string(' ', Console.WindowWidth));
-                }
+                return false;
             }
-            Console.Clear();
-            return string.Empty;
-        }
-        public int GetMovieId(string title)
-        {
-            var movie = _context.Movies.FirstOrDefault(m => m.Title == title);
-            if (movie == null)
+            var likedMovie = new LikedMovie
             {
-                return -1;
-            }
+                UserId = userId,
+                MovieId = movieId
+            };
 
-            return movie.Id;
-        }
-        public void AddToLikedMovies(string username, string movieTitle)
-        {
-            Console.Clear();
-            var user = _context.Users.FirstOrDefault(u => u.Username == username);
-            var movieId = new MovieController(_context).GetMovieId(movieTitle);
-
-            if (movieId == -1)
-            {
-                return;
-            }
-
-            if (user != null && movieId != 0)
-            {
-                var alreadyLiked = _context.LikedMovies.Any(lm => lm.UserId == user.Id && lm.MovieId == movieId);
-
-                if (!alreadyLiked)
-                {
-                    var likedMovie = new LikedMovie
-                    {
-                        UserId = user.Id,
-                        MovieId = movieId
-                    };
-                    _context.LikedMovies.Add(likedMovie);
-                    _context.SaveChanges();
-                    Console.WriteLine("The movie has been added to the liked movies.");
-                }
-                else
-                {
-                    Console.WriteLine("The movie is already in your favorites.");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Please, try again.");
-            }           
+            _context.LikedMovies.Add(likedMovie);
+            _context.SaveChanges();
+            return true;
         }
 
         public List<Movie> GetLikedMovies(int userId)
@@ -207,41 +80,36 @@ namespace OcenaFilmow.Controllers
                 .Select(u => u.Movie)
                 .ToList();
         }
-        public void AddToWatchList(string username, string movieTitle)
+        public bool RemoveFromLiked(int userId, int movieId)
         {
-            Console.Clear();
-            var user = _context.Users.FirstOrDefault(u => u.Username == username);
-            var movieId = new MovieController(_context).GetMovieId(movieTitle);
+            var likedListEntry = _context.LikedMovies.FirstOrDefault(w => w.UserId == userId && w.MovieId == movieId);
 
-            if (movieId == -1)
+            if (likedListEntry != null)
             {
-                return;
+                _context.LikedMovies.Remove(likedListEntry);
+                _context.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+        public bool AddToWatchList(int userId, int movieId)
+        {
+            bool alreadyLiked = _context.LikedMovies.Any(lm => lm.UserId == userId && lm.MovieId == movieId);
+
+            if (alreadyLiked)
+            {
+                return false;
             }
 
-            if (user != null && movieId != 0)
+            var movie = new Watchlist
             {
-                var alreadyAdded = _context.Watchlists.Any(lm => lm.UserId == user.Id && lm.MovieId == movieId);
+                UserId = userId,
+                MovieId = movieId
+            };
 
-                if (!alreadyAdded)
-                {
-                    var watchList = new Watchlist
-                    {
-                        UserId = user.Id,
-                        MovieId = movieId
-                    };
-                    _context.Watchlists.Add(watchList);
-                    _context.SaveChanges();
-                    Console.WriteLine("The movie has been added to the watchlist.");
-                }
-                else
-                {
-                    Console.WriteLine("The movie is already in your watchlist.");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Please, try again.");
-            }
+            _context.Watchlists.Add(movie);
+            _context.SaveChanges();
+            return true;
         }
         public List<Movie> GetWatchList(int userId)
         {
@@ -250,6 +118,18 @@ namespace OcenaFilmow.Controllers
                 .Include(u => u.Movie)
                 .Select(u => u.Movie)
                 .ToList();
+        }
+        public bool RemoveFromWatchlist(int userId, int movieId)
+        {
+            var watchlistEntry = _context.Watchlists.FirstOrDefault(w => w.UserId == userId && w.MovieId == movieId);
+
+            if (watchlistEntry != null)
+            {
+                _context.Watchlists.Remove(watchlistEntry);
+                _context.SaveChanges();
+                return true;
+            }
+            return false;
         }
         public List<string> GetAllGenres()
         {
@@ -298,7 +178,6 @@ namespace OcenaFilmow.Controllers
                     .Where(movie => movie.Rating >= minRating.Value)
                     .ToList();
             }
-
             if (maxRating.HasValue)
             {
                 filteredMovies = filteredMovies
@@ -308,35 +187,6 @@ namespace OcenaFilmow.Controllers
 
             return filteredMovies;
         }
-        public static List<Movie> ApplyFilters(MovieController movieController, List<Movie> movies)
-        {
-            var genreChoices = movieController.GetAllGenres();
-            var selectedGenres = AnsiConsole.Prompt(
-                new MultiSelectionPrompt<string>()
-                    .Title("Select genres (use arrows and space to select):")
-                    .PageSize(10)
-                    .AddChoices(genreChoices));
 
-            int? startYear = AnsiConsole.Confirm("Do you want to specify a start year?")
-                ? AnsiConsole.Ask<int>("Enter start year:")
-                : (int?)null;
-
-            int? endYear = AnsiConsole.Confirm("Do you want to specify an end year?")
-                ? AnsiConsole.Ask<int>("Enter end year:")
-                : (int?)null;
-
-            double? minRating = AnsiConsole.Confirm("Do you want to specify a minimum rating?")
-                ? AnsiConsole.Ask<double>("Enter minimum rating:")
-                : (double?)null;
-
-            double? maxRating = AnsiConsole.Confirm("Do you want to specify a maximum rating?")
-                ? AnsiConsole.Ask<double>("Enter maximum rating:")
-                : (double?)null;
-
-            var filteredMovies = movieController.FilterMovies(movies, selectedGenres, startYear, endYear, minRating, maxRating);
-
-            Console.WriteLine($"DEBUG: Filtered movies count: {filteredMovies.Count}");
-            return filteredMovies;
-        }
     }
 }
